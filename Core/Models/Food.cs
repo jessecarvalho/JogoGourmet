02e37af95a349b77e620961b91a7720b31b3fb26
Jessé Carvalho <jesseelias80@gmail.com>
2024-01-06 15:42:12 -0300

    Fix Models

--- Core/Models/Food.cs
+++ Core/Models/Food.cs
@@ -4,5 +4,10 @@ namespace Core.Models;
 public class Food
 {
     public string name { get; set; }
-    public Characteristic characteristic { get; set; }
+    public Characteristic? characteristic { get; set; }
+    
+    public Food(string name)
+    {
+        this.name = name;
+    }
 }
