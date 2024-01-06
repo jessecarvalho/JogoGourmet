d03808c9de7a0c086b74b9a73c05014e5a44b5af
Jessé Carvalho <jesseelias80@gmail.com>
2024-01-06 15:29:37 -0300

    Initial implementation for GameService

--- Core/Services/GameService.cs
+++ Core/Services/GameService.cs
@@ -1,6 +1,37 @@
-namespace DefaultNamespace;
+using Core.Interfaces;
+using Core.Models;
 
-public class Game
+namespace Core.Services;
+
+public class GameService: IGame
 {
-    
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TryToGuess()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GuessedRight()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Food AddNewFood()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Characteristic AddNewCharacteristic()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddNewFoodToNewCharacteristic()
+    {
+        throw new NotImplementedException();
+    }
 }
