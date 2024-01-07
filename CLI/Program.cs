3db8aec3ade69482942f2bafa6d32c4002fca2eb
Jessé Carvalho <jesseelias80@gmail.com>
2024-01-07 15:21:16 -0300

    [refactor]: Some general refactoring

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Core.Models;
+using Core.Services;
+
+namespace Core.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gameService = new GameService(); 
+            var game = new Game(gameService); 
+            game.Start();
+        }
+    }
+}
