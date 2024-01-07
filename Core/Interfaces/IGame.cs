1758538286dddded03a648bf3e649e9d5edf289d
Jessé Carvalho <jesseelias80@gmail.com>
2024-01-07 15:15:25 -0300

    [fix]: Changing IGame

--- Core/Interfaces/IGame.cs
+++ Core/Interfaces/IGame.cs
@@ -5,10 +5,10 @@ namespace Core.Interfaces;
 public interface IGame
 {
     public void Start();
-    public void AddInitialFoodsAndCharacteristics();
-    public void TryToGuess();
+    public void ResetGuessedState(FoodNode node);
+
+    public void TryToGuess(FoodNode node, ref bool guessedRight, ref bool endSearch, ref FoodNode lastGuessedNode,
+        bool exploreChild = false);
     public void GuessedRight();
-    public FoodNode AddNewFood();
-    public FoodNode AddNewCharacteristic();
-    public void AddNewFoodToNewCharacteristic();
+    public FoodNode AddNewFoodNode(FoodNode node);
 }
