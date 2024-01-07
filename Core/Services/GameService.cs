4b423f42765aeb801443c58d7100812d543d203f
Jessé Carvalho <jesseelias80@gmail.com>
2024-01-07 15:17:12 -0300

    [fix] Adaptation for GameService

--- Core/Services/GameService.cs
+++ Core/Services/GameService.cs
@@ -3,35 +3,49 @@ using Core.Models;
 
 namespace Core.Services;
 
-public class GameService: IGame
+public class GameService : IGameService
 {
-    public void Start()
+
+    public FoodNode AddNewFoodNode(FoodNode node, string newFood, string newCharacteristic)
     {
-        throw new NotImplementedException();
+        var newFoodNode = new FoodNode(newFood);
+        var newCharacteristicNode = new FoodNode(newCharacteristic);
+        newCharacteristicNode.AddNode(newFoodNode);
+        node.AddNode(newCharacteristicNode);
+        return newCharacteristicNode;
     }
 
-    public void TryToGuess()
+    public FoodNode AddInitialFoodsAndCharacteristics()
     {
-        throw new NotImplementedException();
+        var initialNode = new FoodNode("Initial Node");
+        var pasta = new FoodNode("Massa");
+        pasta.AddNode(new FoodNode("Lasanha"));
+        initialNode.AddNode(pasta);
+        var chocolateCake = new FoodNode("Bolo de Chocolate");
+        initialNode.AddNode(chocolateCake);
+        return initialNode;
     }
 
-    public void GuessedRight()
+    public bool ReadBool()
     {
-        throw new NotImplementedException();
+        var answer = Console.ReadLine();
+        
+        while (answer?.ToUpper() != "S" && answer?.ToUpper() != "N")
+        {
+            Console.WriteLine("Resposta inválida. Digite S ou N.");
+            answer = Console.ReadLine();
+        }
+        
+        switch (answer?.ToUpper())
+        {
+            case "S":
+                return true;
+            case "N":
+                return false;
+            default:
+                return false;
+        }
+        
     }
 
-    public Food AddNewFood()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Characteristic AddNewCharacteristic()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void AddNewFoodToNewCharacteristic()
-    {
-        throw new NotImplementedException();
-    }
 }
